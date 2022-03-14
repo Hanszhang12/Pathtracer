acfa1acd99d7094b741e87cb85173febfd295f7b
Bilal Syed <47010415+bilalsyed7@users.noreply.github.com>
2022-03-13 19:36:53 -0700

    1.3/4 complete (may need to debug once completing 1.2)

--- src/scene/sphere.cpp
+++ src/scene/sphere.cpp
@@ -14,10 +14,28 @@ bool Sphere::test(const Ray &r, double &t1, double &t2) const {
   // Implement ray - sphere intersection test.
   // Return true if there are intersections and writing the
   // smaller of the two intersection times in t1 and the larger in t2.
-
-
-  return true;
-
+  double r_mag = dot(r.d, r.d), og_dir = 2 * dot((r.o - this->o), r.d), ogmag_rad = dot(r.o - this->o, r.o - this->o) - this->r2;
+  double diff = (og_dir * og_dir) - (4 * r_mag * ogmag_rad);
+  if(diff <= 0) {
+    return false;
+  }
+  else {
+    double t_low = (-og_dir - sqrt(diff)) / 2 / r_mag, t_high = (-og_dir + sqrt(diff)) / 2 / r_mag;
+    if(t_low >= r.min_t && t_high <= r.max_t) {
+      t1 = t_low;
+      t2 = t_high;
+      if (t_low > 0) {
+        r.max_t = t_low;
+      }
+      else {
+        r.max_t = t_high;
+      }
+      return true;
+    }
+    return false;
+  }
+  //should not reach this point
+  return false;
 }
 
 bool Sphere::has_intersection(const Ray &r) const {
@@ -25,9 +43,8 @@ bool Sphere::has_intersection(const Ray &r) const {
   // TODO (Part 1.4):
   // Implement ray - sphere intersection.
   // Note that you might want to use the the Sphere::test helper here.
-
-
-  return true;
+  double t1, t2;
+  return this->test(r, t1, t2);
 }
 
 bool Sphere::intersect(const Ray &r, Intersection *i) const {
@@ -37,10 +54,18 @@ bool Sphere::intersect(const Ray &r, Intersection *i) const {
   // Note again that you might want to use the the Sphere::test helper here.
   // When an intersection takes place, the Intersection data should be updated
   // correspondingly.
-
-
-
-  return true;
+  if(!has_intersection(r)) {
+    return false;
+  }
+  else {
+    Vector3D prenorm = (r.max_t * r.d + r.o) - this->o;
+    i->n = prenorm;
+    prenorm.normalize();
+    i->t = r.max_t;
+    i->bsdf = get_bsdf();
+    i->primitive = this;
+    return true;
+  }
 }
 
 void Sphere::draw(const Color &c, float alpha) const {
--- src/scene/triangle.cpp
+++ src/scene/triangle.cpp
@@ -27,21 +27,37 @@ bool Triangle::has_intersection(const Ray &r) const {
   // The difference between this function and the next function is that the next
   // function records the "intersection" while this function only tests whether
   // there is a intersection.
-
-
+  Vector3D edge_1 = p2 - p1, edge_2 = p3 - p1, ray_edge = r.o - p1;
+  Vector3D s1 = cross(r.d, edge_2), s2 = cross(ray_edge, edge_1);
+  Vector3D dots = Vector3D(dot(s2, edge_2), dot(s1, ray_edge), dot(s2, r.d));
+  Vector3D fin = 1 / dot(s1, edge_1) * dots;
+  double this_t = fin.x, bar1 = fin.y, bar2 = fin.z;
+  if(bar1 < 0 || bar2 < 0 || bar1 + bar2 > 1 || this_t < r.min_t || this_t > r.max_t) {
+    return false;
+  }
+  r.max_t = this_t;
   return true;
-
 }
 
 bool Triangle::intersect(const Ray &r, Intersection *isect) const {
   // Part 1, Task 3:
   // implement ray-triangle intersection. When an intersection takes
   // place, the Intersection data should be updated accordingly
-
-
+  Vector3D edge_1 = p2 - p1, edge_2 = p3 - p1, ray_edge = r.o - p1;
+  Vector3D s1 = cross(r.d, edge_2), s2 = cross(ray_edge, edge_1);
+  Vector3D dots = Vector3D(dot(s2, edge_2), dot(s1, ray_edge), dot(s2, r.d));
+  Vector3D fin = 1 / dot(s1, edge_1) * dots;
+  double this_t = fin.x, bar1 = fin.y, bar2 = fin.z;
+  if(bar1 < 0 || bar2 < 0 || bar1 + bar2 > 1 || this_t < r.min_t || this_t > r.max_t) {
+    return false;
+  }
+  double bar3 = 1.0 - bar1 - bar2;
+  r.max_t = this_t;
+  isect->n = n1 * bar1 + n2 * bar2 + n3 * bar3;
+  isect->t = this_t;
+  isect->bsdf = get_bsdf();
+  isect->primitive = this;
   return true;
-
-
 }
 
 void Triangle::draw(const Color &c, float alpha) const {
