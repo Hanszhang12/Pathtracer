e31cda0ecbc61cda793a9096329484b128856c07
Bilal Syed <47010415+bilalsyed7@users.noreply.github.com>
2022-03-17 10:16:06 -0700

    Fixed 1.4 (smooth contours now)

--- src/scene/sphere.cpp
+++ src/scene/sphere.cpp
@@ -14,22 +14,18 @@ bool Sphere::test(const Ray &r, double &t1, double &t2) const {
   // Implement ray - sphere intersection test.
   // Return true if there are intersections and writing the
   // smaller of the two intersection times in t1 and the larger in t2.
-  double r_mag = dot(r.d, r.d), og_dir = 2 * dot((r.o - this->o), r.d), ogmag_rad = dot(r.o - this->o, r.o - this->o) - this->r2;
+  double r_mag = dot(r.d, r.d), og_dir = 2.0f * dot((r.o - this->o), r.d), ogmag_rad = dot(r.o - this->o, r.o - this->o) - this->r2;
   double diff = (og_dir * og_dir) - (4 * r_mag * ogmag_rad);
   if(diff <= 0) {
     return false;
   }
   else {
     double t_low = (-og_dir - sqrt(diff)) / 2 / r_mag, t_high = (-og_dir + sqrt(diff)) / 2 / r_mag;
-    if(t_low >= r.min_t && t_high <= r.max_t) {
-      t1 = t_low;
-      t2 = t_high;
-      if (t_low > 0) {
-        r.max_t = t_low;
-      }
-      else {
-        r.max_t = t_high;
-      }
+    t1 = t_high;
+    t2 = t_low;
+    double mint = min(t1, t2);
+    if(mint >= r.min_t && mint <= r.max_t) {
+      r.max_t = mint;
       return true;
     }
     return false;
@@ -48,20 +44,20 @@ bool Sphere::has_intersection(const Ray &r) const {
 }
 
 bool Sphere::intersect(const Ray &r, Intersection *i) const {
-
   // TODO (Part 1.4):
   // Implement ray - sphere intersection.
   // Note again that you might want to use the the Sphere::test helper here.
   // When an intersection takes place, the Intersection data should be updated
   // correspondingly.
-  if(!has_intersection(r)) {
+  double t1, t2;
+  if(!test(r, t1, t2)) {
     return false;
   }
   else {
-    Vector3D prenorm = (r.max_t * r.d + r.o) - this->o;
-    i->n = prenorm;
+    Vector3D prenorm = (t2 * r.d + r.o) - this->o;
     prenorm.normalize();
-    i->t = r.max_t;
+    i->n = prenorm;
+    i->t = t1;
     i->bsdf = get_bsdf();
     i->primitive = this;
     return true;
